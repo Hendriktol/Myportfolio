5cb8a627f6970d898af1a1a96b91b1abb52912cc
Hendriktol <thevartialenvironment@gmail.com>
2020-10-22 14:52:32 +0200

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,13 +1,18 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
     <head>
         <title>Hendrik Tol</title>
         <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+        <meta charset="UTF-8" />
+        <meta name="description" content="Graphic designer for Team Liquid." />
+		<meta name="keywords" content="TEAMLIQUID, TLWIN, tl, teamliquid, liquid, graphic design, design, graphics, livestream, gaming" />
         <link rel="stylesheet" href="{{ asset('css/app.css') }}"/>
-        <script type='text/javascript' src='{{ asset('js/app.js') }}'></script>
+        <meta name="theme-color" content="#163552" />
     </head>
     <body>
         @include('layouts.navbar')
         @yield('content')
         @include('layouts.footer')
+        <script type='text/javascript' src='{{ asset('js/app.js') }}'></script>
     </body>
 </html>
